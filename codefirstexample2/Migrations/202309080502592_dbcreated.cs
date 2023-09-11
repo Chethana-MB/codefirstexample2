bef024a3ef5d3687b33aefa7c5841e9aa6be9568
student <student@MB>
2023-09-11 11:32:14 +0530

    Add project files.

---
+++ codefirstexample2/Migrations/202309080502592_dbcreated.cs
@@ -0,0 +1,43 @@
+﻿namespace codefirstexample2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dbcreated : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        catename = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        fullprice = c.Single(nullable: false),
+                        category_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.categories", t => t.category_Id)
+                .Index(t => t.category_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courses", "category_Id", "dbo.categories");
+            DropIndex("dbo.Courses", new[] { "category_Id" });
+            DropTable("dbo.Courses");
+            DropTable("dbo.categories");
+        }
+    }
+}
---
+++ codefirstexample2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codefirstexample2
+{
+    public class category
+    {
+        public int Id { set; get; }
+        public string catename { set; get; }
+    }
+    public class Course
+    {
+        public int Id { set; get; }
+        public string Title { set; get; }
+        public string Description { set; get; }
+        public float fullprice { set; get; }
+
+        public category category { set; get; }
+    }
+
+    public class PlutoContext : DbContext
+    {
+        public DbSet<Course> Courses { set; get; }
+        public DbSet<category> cats { set; get; }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
